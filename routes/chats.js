c1c4a3eba407975bdd35bff7568a59f52c2c70f7
Nikita Gamaiunov <Habatynchik@gmail.com>
2025-04-26 16:45:47 +0300

    made some changes

--- routes/chats.js
+++ routes/chats.js
@@ -3,22 +3,42 @@ let router = express.Router();
 
 let chatService = require('../services/chatService');
 
-router.post('/create', function(req, res, next) {
+router.post('/create', function (req, res, next) {
+    let chat = {
+        name: req.body.name,
+        description: req.body.description,
+        logo_url: req.body.logo_url,
+    }
+    chatService.createChat(chat)
+        .then((chat) => {
+            res.send(chat);
+        })
+        .catch((error) => {
+            res.send(error);
+        })
 })
-router.put('/:id/update', function(req, res, next) {
+router.put('/:id/update', function (req, res, next) {
+    let chat = {
+        id: req.params.id,
+        name: req.body.name,
+        description: req.body.description,
+        logo_url: req.body.logo_url,
+    }
+    chatService.updateChat(chat)
+        .then((chat) => {
+            res.send(chat);
+        })
+        .catch((error) => {
+            res.send(error);
+        })
 })
-router.delete('/:id/delete', function(req, res, next) {
+router.delete('/:id/delete', function (req, res, next) {
 })
-router.post('/:id/add-member', function(req, res, next) {
+router.post('/:id/add-member', function (req, res, next) {
 })
-
-/*
-    create chat
-    update chat
-    delete chat
-    add member to chat
-    remove member from chat
-    get chat members
- */
+router.delete('/:id/remove-member', function (req, res, next) {
+})
+router.get('/:id/members', function (req, res, next) {
+})
 
 module.exports = router;
--- services/chatService.js
+++ services/chatService.js
@@ -6,6 +6,10 @@ const chatService = {
         chat =  await chatRepository.createChat(chat);
         return chat
     },
+    updateChat: async (chat) => {
+        chat =  await chatRepository.updateChat(chat);
+        return chat
+    },
     addMemberIntoChat: async (memberId, chatId) => {
        return await chatMembersRepository.addMemberIntoChat(chatId, memberId);
     },
